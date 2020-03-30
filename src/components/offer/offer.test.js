d4b7ae66bef5a243252a8388e02dc9faec78faae
alex_molodoy <sivtsov_a@inbox.ru>
2020-03-30 14:15:49 +0300

    module4-task1v1

--- src/components/offer/offer.test.js
+++ src/components/offer/offer.test.js
@@ -6,6 +6,7 @@ import Offer from './offer.jsx';
 it(`Should render Offer correctly`, () => {
   const offer = {
     bedrooms: 1,
+    coords: [52.3909553943508, 4.85309666406198],
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing enim eu turpis egestas pretium aenean pharetra magna. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus.`,
     features: [`Wifi`, `Heating`, `Cable TV`, `Fridge`, `Coffee machine`, `Towels`],
     guests: 2,
